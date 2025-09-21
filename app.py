fe3cfa07d0d596970e0580ee18f7ad43fab09c81
hyoond <hoeyoond@gmail.com>
2025-09-21 18:18:58 +0800

    Fully working flask codes

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.secret_key = 'secretkey123'
 
-@app.route('/homepage', methods=["GET"])
+@app.route("/")
+def start():
+    username = ""
+    return render_template ("homepage.html")
+
+@app.route('/homepage', methods=["POST"])
 def homepage():
     username = request.form["username"]
-    return render_template("homepage.html", username=username)
+    return render_template ("preferences.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
